b2e5fb637cbe75c910ba2c66c4cae134ec1c8f71
Daniel Ruiz <danielruimar@gmail.com>
2023-05-19 19:57:00 +0200

    Added Product and User models

---
+++ models/User.js
@@ -0,0 +1,59 @@
+//  Importamos Mongoose
+const mongoose = require("mongoose");
+
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
+// Declaramos nuestro esquema que nos permite declarar nuestros objetos y crearle restricciones.
+const Schema = mongoose.Schema;
+
+// Creamos esquema del user:
+
+const allowedCountries = ["SPAIN", "COLOMBIA", "ENGLAND", "RUSSIA", "ENGLAND", "UNITED STATES", "ARGENTINA", "CZECHOSLOVAKIA", "UNITED STATES", "JAPAN", "NIGERIA"];
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      unique: true, // indica que no puede haber otra entidad con esta propiedad que tenga el mismo valor.
+      validate: {
+        validator: validator.isEmail, // Validamos haciendo uso de la librería validator y la función isEmail que incorpora.
+        message: "Email incorrecto",
+      },
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      unique: true,
+      minLength: [8, "La contraseña debe tener al menos 8 caracteres"],
+      select: false, // Indica que no lo deseamos mostrar cuando se realicen las peticiones.
+      required: true,
+    },
+    name: { type: String, trim: true, minLength: [3, "Al menos tres letras para el nombre"], maxLength: [22, "Nombre demasiado largo, máximo de 22 caracteres"], required: true },
+  },
+  { timestamps: true } // Cada vez que se modifique un documento refleja la hora y fecha de modificación
+);
+
+// Cada vez que se guarde un usuario encriptamos la contraseña
+userSchema.pre("save", async function (next) {
+  try {
+    // Si la password estaba encriptada, no la encriptaremos de nuevo.
+    if (this.isModified("password")) {
+      // Si el campo password se ha modificado
+      const saltRounds = 10;
+      const passwordEncrypted = await bcrypt.hash(this.password, saltRounds); // Encriptamos la contraseña
+      this.password = passwordEncrypted; // guardamos la password en la entidad User
+      next();
+    }
+  } catch (error) {
+    next();
+  }
+});
+
+// Creamos un modelo para que siempre que creamos un user valide contra el Schema que hemos creado para ver si es valido.
+const User = mongoose.model("User", userSchema);
+
+//  Exportamos el modelo para poder usarlo fuera.
+module.exports = { User };
