d3d7bd9384d297501d65bd41cf40d49c81fe6c73
Daniel Fernandez <dfernandezcode@gmail.com>
2023-05-20 11:33:36 +0200

    resetProducts fixed

--- utils/resetProducts.js
+++ utils/resetProducts.js
@@ -1,23 +1,23 @@
 const { Product } = require("../models/Product.js");
 const { User } = require("../models/User.js");
 
-const generateRandom = require("../utils/generateRandom.js");
+const { generateRandom } = require("../utils/generateRandom.js");
 
 const productList = [
-  { name: "Jersey Zara L", price: "€20", description: "negro con lineas azules" },
-  { name: "T-Shirt Adidas M", price: "€15", description: "blanca con logo negro" },
-  { name: "Jeans Levi's 501", price: "€50", description: "azul oscuro, estilo recto" },
-  { name: "Sneakers Nike Air Max", price: "€100", description: "negro con suela blanca" },
-  { name: "Dress H&M", price: "€30", description: "estampado floral, estilo camisero" },
-  { name: "Watch Casio", price: "€50", description: "correa de acero inoxidable, color plateado" },
-  { name: "Backpack Eastpak", price: "€40", description: "negra, con múltiples compartimentos" },
-  { name: "Sunglasses Ray-Ban", price: "€80", description: "montura de metal dorado, lentes polarizadas" },
-  { name: "Perfume Chanel No.5", price: "€120", description: "fragancia clásica y elegante" },
-  { name: "Wallet Tommy Hilfiger", price: "€35", description: "cuero marrón, múltiples ranuras para tarjetas" },
-  { name: "Earrings Swarovski", price: "€60", description: "pendientes de cristal con forma de gota" },
+  { name: "Jersey Zara L", price: 20, description: "negro con lineas azules" },
+  { name: "T-Shirt Adidas M", price: 15, description: "blanca con logo negro" },
+  { name: "Jeans Levi's 501", price: 50, description: "azul oscuro, estilo recto" },
+  { name: "Sneakers Nike Air Max", price: 100, description: "negro con suela blanca" },
+  { name: "Dress H&M", price: 30, description: "estampado floral, estilo camisero" },
+  { name: "Watch Casio", price: 50, description: "correa de acero inoxidable, color plateado" },
+  { name: "Backpack Eastpak", price: 40, description: "negra, con múltiples compartimentos" },
+  { name: "Sunglasses Ray-Ban", price: 80, description: "montura de metal dorado, lentes polarizadas" },
+  { name: "Perfume Chanel No.5", price: 120, description: "fragancia clásica y elegante" },
+  { name: "Wallet Tommy Hilfiger", price: 35, description: "cuero marrón, múltiples ranuras para tarjetas" },
+  { name: "Earrings Swarovski", price: 60, description: "pendientes de cristal con forma de gota" },
 ];
 
-const productSeed = async () => {
+const resetProducts = async () => {
   try {
     // Borrar datos
     await Product.collection.drop();
@@ -30,10 +30,11 @@ const productSeed = async () => {
     }
 
     for (let i = 0; i < productList.length; i++) {
-      const product = new Product(productList[i]);
-      product.owner = users[generateRandom(0, users.length)];
-
-      await product.save();
+      const product = productList[i];
+      product.owner = users[generateRandom(0, users.length - 1)]._id;
+      console.log(product.owner);
+      const document = new Product(product);
+      await document.save();
     }
 
     // Añadimos Productos
@@ -49,4 +50,4 @@ const productSeed = async () => {
   }
 };
 
-module.exports = { productSeed };
+module.exports = { resetProducts };
