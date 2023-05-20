3d470035532ea432d28a9d128442a5f614ebf9eb
Daniel Ruiz <danielruimar@gmail.com>
2023-05-20 12:59:28 +0200

    Fix login endpoint

---
+++ middlewares/checkParams.middleware.js
@@ -0,0 +1,27 @@
+const { User } = require("../models/User");
+const { verifyToken } = require("../utils/token");
+
+// Middleware previo al get de autores para comprobar los parametros:
+
+const checkParams = async (req, res, next) => {
+  try {
+    console.log("Estamos en el Middleware que comprueba los parámetros");
+
+    const page = req.query.page ? parseInt(req.query.page) : 1;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 10;
+
+    if (!isNaN(page) && !isNaN(limit) && page > 0 && limit > 0) {
+      req.query.page = page;
+      req.query.limit = limit;
+      next();
+    } else {
+      console.log("Parametros no validos:");
+      console.log(JSON.stringify(req.query));
+      res.status(400).json({ error: "Params are not valid" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { checkParams };
